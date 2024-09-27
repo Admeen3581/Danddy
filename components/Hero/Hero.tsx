19ea992c3ee42d2e8d1ca3de2299a1b5dbe5d9e9
Lorenz De Robles <pvd8@txstate.edu>
2024-09-27 16:59:26 -0500

    Added the hero section for the main page

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Welcome to Danddy</h1>
+        <p>Sruff here</p>
+        <button className="hero-button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
