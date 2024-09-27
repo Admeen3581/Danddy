8f05ed0642c529a1e8430d33eebc41b3a3c3e5da
Lorenz De Robles <pvd8@txstate.edu>
2024-09-27 17:01:11 -0500

    Added the component in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "../../components/Hero/Hero";
 import NavBar from "../../components/NavigationBar/NavBar";
 
 export default function Home() {
   return (
-    <NavBar/>
+    <>
+    <NavBar />
+    <Hero />
+    </>
   );
 }
