d1f89e0d906f5499771c8e3bd1d941b4662eec44
Olivia Laurel <zsw26@txstate.edu>
2024-09-28 18:38:39 -0500

    Added in button links and dm/player buttons

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import Link from 'next/link';
 
 const Hero = () => {
   return (
@@ -7,7 +8,12 @@ const Hero = () => {
       <div className="hero-content">
         <h1>Welcome to Danddy</h1>
         <p>Stuff here</p>
-        <button className="hero-button">Get Started</button>
+        <Link href="/dmhome" className="hero-button">
+            DM Start
+          </Link>
+          <Link href="/dmhome" className="hero-button">
+            Player Start
+          </Link>
       </div>
     </div>
   );
