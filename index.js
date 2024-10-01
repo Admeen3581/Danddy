c508d07cdfc1d7d8626f1989eca236d753c216f4
Roman <cvu10@txstate.edu>
2024-09-30 20:24:47 -0500

    Added the indexing form to attach Firebase to our code

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFCTpqWVTM23AwvxBPGVPh8pBk8bEnWU0",
+  authDomain: "danddy-23d02.firebaseapp.com",
+  projectId: "danddy-23d02",
+  storageBucket: "danddy-23d02.appspot.com",
+  messagingSenderId: "557498398280",
+  appId: "1:557498398280:web:2479ff98296b86db02ca7d",
+  measurementId: "G-9E6NEW0EHY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
