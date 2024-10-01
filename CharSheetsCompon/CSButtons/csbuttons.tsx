5ce3dc4ba95c287a912fa9125582aef8506eb806
Lorenz De Robles <pvd8@txstate.edu>
2024-09-30 23:58:29 -0500

    Removed a div and changed name to fix a style loading bug

--- CharSheetsCompon/CSButtons/csbuttons.tsx
+++ CharSheetsCompon/CSButtons/csbuttons.tsx
@@ -3,14 +3,12 @@ import './csbuttons.css';
 
 const CSButtons = () => {
   return (
-    <div className="charsheets-section">
-       <div className="ButtonsBox">
-          <h1>Characters:</h1>
-          <button className='button'>Character 1</button>
-          <button className='button'>Character 2</button>
-          <button className='button'>Character 3</button>
-       </div>
-    </div>
+     <div className="bbx">
+        <h1>Characters:</h1>
+        <button className='button'>Character 1</button>
+        <button className='button'>Character 2</button>
+        <button className='button'>Character 3</button>
+     </div>
   );
 };
 
