82a3fbc837a506efb4033afc31221a9111b751a7
Joshua Mason <kon9@txstate.edu>
2024-10-01 10:34:44 -0500

    Changed location of reference code

--- src/app/dmhome/page.tsx
+++ src/app/dmhome/page.tsx
@@ -5,26 +5,6 @@ import DMNotes from '../../../DmComponents/DMNotes/dmnotes';
 import { createRoom, deleteDatabaseRoute, generateRoomCode, readDatabaseRoute, updateDatabaseRoute } from '@/utils/httpRequester';
 
 const DMHome = async () => {
-
-  // const roomCode = generateRoomCode();  // Generate a random room code
-  // const roomData = {
-  //     host: "John Doe",
-  //     players: {},
-  //     settings: {
-  //         maxPlayers: 4,
-  //         isPrivate: true
-  //     }
-  // };
-
-  // await createRoom(roomCode, roomData);
-
-  // await updateDatabaseRoute("rooms/AAAAAA/players", roomData)
-  // await updateDatabaseRoute("rooms/BBB/players", roomData)
-
-  // console.log(readDatabaseRoute("rooms/AAAAAA/players/host"))
-
-  // await deleteDatabaseRoute("rooms/4QFY8Z")
-
   return (
    <>
    <DMHeader />
