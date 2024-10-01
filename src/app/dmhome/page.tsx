1b3ca6f0c79a6057ddf8e72ec357fc6335faa8f7
Lorenz De Robles <pvd8@txstate.edu>
2024-10-01 15:25:51 -0500

    Added Linting comments to compile the code

--- src/app/dmhome/page.tsx
+++ src/app/dmhome/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DMHeader from '../../../DmComponents/dmHeader/dmheader';
 import DMButtons from '../../../DmComponents/DMButtons/dmbuttons';
 import DMNotes from '../../../DmComponents/DMNotes/dmnotes';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createRoom, deleteDatabaseRoute, generateRoomCode, readDatabaseRoute, updateDatabaseRoute } from '@/utils/httpRequester';
 
 const DMHome = async () => {
