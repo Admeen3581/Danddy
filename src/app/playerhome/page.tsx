789d58a19338cfb48d78706a8d3b85299da249dd
Olivia Laurel <zsw26@txstate.edu>
2024-09-30 21:57:57 -0500

    CP-82 Creating page and route for Player UI

---
+++ src/app/playerhome/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlayerHeader from '../../../PhComp/PlayerHeader/playerheader';
+
+
+const DMHome = () => {
+  return (
+   <>
+      <PlayerHeader />
+   </>
+  );
+};
+
+export default DMHome;
