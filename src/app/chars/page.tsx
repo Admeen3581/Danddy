55707017bd133a80523d77720a9036cfc1e99572
Lorenz De Robles <pvd8@txstate.edu>
2024-10-01 00:03:54 -0500

    Created containers for the components

--- src/app/chars/page.tsx
+++ src/app/chars/page.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import CSHeader from '../../../CharSheetsCompon/CSHeader/csheader';
 import CSButtons from '../../../CharSheetsCompon/CSButtons/csbuttons';
 import CSStats from '../../../CharSheetsCompon/CSStats/csstats';
+import CSNotes from '../../../CharSheetsCompon/CSNotes/csnotes';
+import './styles.css'; 
 
 const CharHome = () => {
   return (
+    <>
     <div className="container">
       <CSHeader />
-      <CSButtons />
+      </div>
+      
+    <div className="stats-container">
+      <CSButtons/>
       <CSStats />
+      <CSNotes/>
     </div>
+    </>
   );
 };
 
