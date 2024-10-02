cccc93179fc2117b97fb574b6214a184231bf463
Roman <cvu10@txstate.edu>
2024-10-02 12:34:33 -0500

    Moved data into a .env file

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,13 +7,14 @@ import { getDatabase, ref, set, onValue } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "danddy-23d02.firebaseapp.com",
-  projectId: "danddy-23d02",
-  storageBucket: "danddy-23d02.appspot.com",
-  messagingSenderId: "557498398280",
-  appId: "1:557498398280:web:2479ff98296b86db02ca7d",
-  measurementId: "G-9E6NEW0EHY"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
