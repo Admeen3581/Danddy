ba0747d84ace042c7b4f6ed9ed347325dab69585
Olivia Laurel <zsw26@txstate.edu>
2024-10-02 00:16:51 -0500

    CP-82 Created Player Notes Box (Fixed Ver.)

---
+++ PlayerHeaderComponents/PlayerNotes/playernotes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './playernotes.css';
+
+const PlayerNotes = () => {
+  return (
+    <div className="PANotes-section">
+       <div className="PANotesBox">
+          <h1>Notes:</h1>
+       </div>
+    </div>
+  );
+};
+
+export default PlayerNotes;
