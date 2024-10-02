1639e58d2c739779a29c35dfd1bf527d91e68cd1
Olivia Laurel <zsw26@txstate.edu>
2024-10-01 23:44:10 -0500

    CP-82 Added in the Player stats bar

---
+++ PlayerHeaderComponents/PlayerStats/playerstats.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './playerstats.css';
+
+const PlayerStats = () => {
+  return (
+    <div className="section">
+       <div className="StatsBox">
+       <span className="inline-text">Strength</span>
+       <span className="inline-text">Dexterity</span>
+       <span className="inline-text">Constitution</span>
+       <span className="inline-text">Intelligence</span>
+       <span className="inline-text">Wisdom</span>
+       <span className="inline-text">Charisma</span>
+       </div>
+    </div>
+  );
+};
+
+export default PlayerStats;
--- src/app/playerhome/page.tsx
+++ src/app/playerhome/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PlayerHeader from '../../../PlayerHeaderComponents/PlayerHeader/playerheader';
+import PlayerStats from '../../../PlayerHeaderComponents/PlayerStats/playerstats';
 
 
 const DMHome = () => {
   return (
    <>
       <PlayerHeader />
+      <PlayerStats />
    </>
   );
 };
