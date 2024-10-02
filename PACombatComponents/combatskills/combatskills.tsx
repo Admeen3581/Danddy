a393db2cfd374e7658e86c52300bd071de0c3b66
Olivia Laurel <zsw26@txstate.edu>
2024-10-02 12:26:56 -0500

    CP-24 Added combat skills component

---
+++ PACombatComponents/combatskills/combatskills.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './combatskills.css';
+
+const CombatSkills = () => {
+  return (
+    <div className="CombatHome-section">
+       <div className="CBSkillsBox">
+            <h1>Skills:</h1>
+       </div>
+    </div>
+  );
+};
+
+export default CombatSkills;
--- src/app/dmhome/page.tsx
+++ src/app/dmhome/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import DMButtons from '../../../DmComponents/DMButtons/dmbuttons';
 import DMNotes from '../../../DmComponents/DMNotes/dmnotes';
-import DMHeader from '../../../DmComponents/dmHeader/dmheader';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createRoom, deleteDatabaseRoute, generateRoomCode, readDatabaseRoute, updateDatabaseRoute } from '@/utils/httpRequester';
+import DMHeader from '../../../DmComponents/DMHeader/dmheader';
 
 const DMHome = async () => {
   return (
