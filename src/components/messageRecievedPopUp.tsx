755d90f01dab4fb7d0541b98cd497b4875df6912
Adam Long <adam.jacob.long@gmail.com>
2024-10-03 16:02:26 -0500

    CP-46 fix DMHome/page.tsx

--- src/components/messageRecievedPopUp.tsx
+++ src/components/messageRecievedPopUp.tsx
@@ -6,21 +6,38 @@
 
 
 import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
-import {Terminal} from "lucide-react";
+import {MessagesSquare} from "lucide-react";
+import {useState} from "react";
+import {Button} from "@/components/ui/button";
 
 export function MessageRecievePopUp()
 {
+    const [isVisible, setVisiblity] = useState(true);
+
+    const handleClose = () => {
+        setVisiblity(false);
+    }
+
     return(
         <>
-            <div className="flex relative h-screen items-center">
-                <Alert>
-                    <Terminal className="h-4 w-4" />
-                    <AlertTitle>Message Received!</AlertTitle>
-                    <AlertDescription>
-                        [Insert message here]
-                    </AlertDescription>
-                </Alert>
-            </div>
+            {isVisible && (
+                <div className="flex items-center justify-center relative">
+                    <Alert className="w-full max-w-md p-7 mt-40">
+                        <MessagesSquare className="h-6 w-6" />
+                        <div className="relative text-center">
+                            <AlertTitle>Message Received!</AlertTitle>
+                            <AlertDescription>
+                                [Insert message here]
+                            </AlertDescription>
+                            <div className='align-right justify-center mt-12'>
+                                <Button onClick={handleClose}>
+                                    Close
+                                </Button>
+                            </div>
+                        </div>
+                    </Alert>
+                </div>
+            )}
         </>
     )
 }
