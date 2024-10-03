90969e25fe93736e755bf6d8b069f65ab263c1a0
Joshua Mason <kon9@txstate.edu>
2024-10-03 13:17:37 -0500

    Removed testing code for quick patch PR

--- src/app/dmhome/page.tsx
+++ src/app/dmhome/page.tsx
@@ -10,30 +10,28 @@ import useLocalStore from '@/utils/store';
 
 const DMHome = () => {
 
-  const {roomId, setRoomId} = useLocalStore()
+  // const {roomId, setRoomId} = useLocalStore()
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    const roomId = generateRoomCode()
-    const roomJson = {
-      "campaign_id": "campaign_id_1",
-      "user_id": "user_id_1",
-      "participants": ["character_id_1", "character_id_2"],
-      "combat_log": [
-        "Thorn casts Fireball on Garrosh",
-        "Garrosh takes 18 damage",
-        "Garrosh swings Greatsword at Thorn",
-        "Thorn dodges the attack"
-      ],
-      "start_time": "2024-09-28T19:00:00Z",
-      "end_time": "2024-09-28T20:15:00Z"
-    };
+  //   const roomId = generateRoomCode()
+  //   const roomJson = {
+  //     "campaign_id": "campaign_id_1",
+  //     "user_id": "user_id_1",
+  //     "participants": ["character_id_1", "character_id_2"],
+  //     "combat_log": [
+  //       "Thorn casts Fireball on Garrosh",
+  //       "Garrosh takes 18 damage",
+  //       "Garrosh swings Greatsword at Thorn",
+  //       "Thorn dodges the attack"
+  //     ],
+  //     "start_time": "2024-09-28T19:00:00Z",
+  //     "end_time": "2024-09-28T20:15:00Z"
+  //   };
 
-    createRoom(roomId, roomJson)
-    setRoomId(roomId);
-  }, [setRoomId]);
-  
-  console.log(roomId)
+  //   createRoom(roomId, roomJson)
+  //   setRoomId(roomId);
+  // }, []);
 
   return (
    <>
