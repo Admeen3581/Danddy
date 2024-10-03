7e3c3391ae6643a45a1f7127a5893be2ccf82514
Olivia Laurel <zsw26@txstate.edu>
2024-10-03 18:07:42 -0500

    CP-24 Fixing

--- src/app/dmhome/page.tsx
+++ src/app/dmhome/page.tsx
@@ -3,7 +3,6 @@ import DMButtons from '../../../DmComponents/DMButtons/dmbuttons';
 import DMNotes from '../../../DmComponents/DMNotes/dmnotes';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createRoom, deleteDatabaseRoute, generateRoomCode, readDatabaseRoute, updateDatabaseRoute } from '@/utils/httpRequester';
-import DMHeader from '../../../DmComponents/DMHeader/dmheader';
 
 const DMHome = async () => {
   return (
