75ae28a952e78e48743b3c00acd66637ca8e89d8
Joshua Mason <kon9@txstate.edu>
2024-10-02 22:40:11 -0500

    Added state setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+
+import useLocalStore from "@/utils/store";
 import Footer from "../../components/Footer/Footer";
 import Hero from "../../components/Hero/Hero";
 import NavBar from "../../components/NavigationBar/NavBar";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const {roomId, setRoomId} = useLocalStore()
+
+  useEffect(() => {
+    setRoomId("123");
+  }, [setRoomId]);
+  
+  console.log(roomId)
+
   return (
     <>
     <NavBar />
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,8 +1,13 @@
 import { create } from 'zustand';
 
-const useStore = create((set) => ({
-  roomId: null,
-  setRoomId: (id: Number) => set({ roomId: id }),
+interface LocalState {
+  roomId: String,
+  setRoomId: (roomId: String) => void
+}
+
+const useLocalStore = create<LocalState>((set) => ({
+  roomId: "",
+  setRoomId: (roomId: String) => set({ roomId }),
 }));
 
-export default useStore;
+export default useLocalStore;
