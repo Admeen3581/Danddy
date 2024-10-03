2f7d253cf81e70526d25a71c00bba325fb5e5b07
Joshua Mason <kon9@txstate.edu>
2024-10-03 11:33:02 -0500

    Added room generation as well as fixing env issues

--- src/app/dmhome/page.tsx
+++ src/app/dmhome/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+"use client"
+
+import React, { useEffect } from 'react';
 import DMButtons from '../../../DmComponents/DMButtons/dmbuttons';
 import DMNotes from '../../../DmComponents/DMNotes/dmnotes';
 import DMHeader from '../../../DmComponents/dmHeader/dmheader';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createRoom, deleteDatabaseRoute, generateRoomCode, readDatabaseRoute, updateDatabaseRoute } from '@/utils/httpRequester';
+import useLocalStore from '@/utils/store';
 
-const DMHome = async () => {
+const DMHome = () => {
+
+  const {roomId, setRoomId} = useLocalStore()
+
+  useEffect(() => {
+
+    const roomId = generateRoomCode()
+    const roomJson = {
+      "campaign_id": "campaign_id_1",
+      "user_id": "user_id_1",
+      "participants": ["character_id_1", "character_id_2"],
+      "combat_log": [
+        "Thorn casts Fireball on Garrosh",
+        "Garrosh takes 18 damage",
+        "Garrosh swings Greatsword at Thorn",
+        "Thorn dodges the attack"
+      ],
+      "start_time": "2024-09-28T19:00:00Z",
+      "end_time": "2024-09-28T20:15:00Z"
+    };
+
+    createRoom(roomId, roomJson)
+    setRoomId(roomId);
+  }, [setRoomId]);
+  
+  console.log(roomId)
+
   return (
    <>
    <DMHeader />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 "use client"
 
-import useLocalStore from "@/utils/store";
+import { configDotenv } from "dotenv";
 import Footer from "../../components/Footer/Footer";
 import Hero from "../../components/Hero/Hero";
 import NavBar from "../../components/NavigationBar/NavBar";
-import { useEffect } from "react";
 
 export default function Home() {
-
-  const {roomId, setRoomId} = useLocalStore()
-
-  useEffect(() => {
-    setRoomId("123");
-  }, [setRoomId]);
-  
-  console.log(roomId)
 
   return (
     <>
