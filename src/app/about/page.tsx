25baf3d151e5bce96e5a215cd896439e92d07fd3
Lorenz De Robles <pvd8@txstate.edu>
2024-10-14 18:17:50 -0500

    SCRUM-130 Added sections about our product

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,29 @@ export default function About(){
         Welcome to Danddy! Our mission is to improve the quality of life for Dungeon Masters (DMs) and players by making gameflow smoother and saving valuable time. 
       </p>
 
+      <section className="mission">
+        <h2>Our Mission</h2>
+        <p>
+            stuff here
+        </p>
+      </section>
+
+      <section className="features">
+        <h2>Key Features</h2>
+        <p>
+            stuff here
+        </p>
+      </section>
+
+      <section className="team">
+        <h2>Meet the Team</h2>
+        <li>Adam Long</li>
+        <li>Olivia Laurel</li>
+        <li>Roman Merlick</li>
+        <li>Joshua Mason</li>
+        <li>Lorenz De Robles</li>
+      </section>
+
     </div>
   );
 };
