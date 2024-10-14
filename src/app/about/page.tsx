ce5113a16a123303d039080a5fc365f6b36a8646
Lorenz De Robles <pvd8@txstate.edu>
2024-10-14 18:12:28 -0500

    SCRUM-130 Added initial page set up

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './about.css';
+
+export default function About(){
+  return (
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to Danddy! Our mission is to improve the quality of life for Dungeon Masters (DMs) and players by making gameflow smoother and saving valuable time. 
+      </p>
+
+    </div>
+  );
+};
