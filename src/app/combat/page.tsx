110dcc81d2577c293007ed46d5a78a31e4f8449b
Lorenz De Robles <pvd8@txstate.edu>
2024-10-15 18:52:39 -0500

    SCRUM-137 Implemented the Dexterity Component UI

--- src/app/combat/page.tsx
+++ src/app/combat/page.tsx
@@ -1,5 +1,6 @@
 import SavingThrows from "../../../components/Combat/Saving/savingThrows";
 import SensesStats from "../../../components/Combat/Senses/sensesStats";
+import DexSkills from "../../../components/Combat/Skills/dexSkills";
 import WisSkills from "../../../components/Combat/Skills/wisSkills";
 import CharacterStats from "../../../components/Combat/Stats/characterStats";
 
@@ -11,6 +12,7 @@ const Combat = () => {
     <SavingThrows/>
     <SensesStats/>
     <WisSkills/>
+    <DexSkills/>
    </>
   );
 };
