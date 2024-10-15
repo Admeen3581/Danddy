db4c6738fc60d2d52c9689a62d60f8ae2a6bace6
Adam Long <adam.jacob.long@gmail.com>
2024-10-15 12:22:25 -0500

    Add features to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,57 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
+			fill: {
+				1: "rgba(255, 255, 255, 0.10)",
+			},
+			bankGradient: "#1034a6",
+			indigo: {
+				500: "#6172F3",
+				700: "#3538CD",
+			},
+			success: {
+				25: "#F6FEF9",
+				50: "#ECFDF3",
+				100: "#D1FADF",
+				600: "#039855",
+				700: "#027A48",
+				900: "#054F31",
+			},
+			pink: {
+				25: "#FEF6FB",
+				100: "#FCE7F6",
+				500: "#EE46BC",
+				600: "#DD2590",
+				700: "#C11574",
+				900: "#851651",
+			},
+			blue: {
+				25: "#F5FAFF",
+				100: "#D1E9FF",
+				500: "#2E90FA",
+				600: "#1570EF",
+				700: "#175CD3",
+				900: "#194185",
+			},
+			red: {
+				1: '#DA2B25',
+			},
+			sky: {
+				1: "#F3F9FF",
+			},
+			black: {
+				1: "#00214F",
+				2: "#344054",
+			},
+			gray: {
+				25: "#FCFCFD",
+				200: "#EAECF0",
+				300: "#D0D5DD",
+				500: "#667085",
+				600: "#475467",
+				700: "#344054",
+				900: "#101828",
+			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
@@ -55,8 +106,8 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+	  }
+	}
   },
 	// eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
