0bf142ae4a8e7ad4e9ac1c5bbc69fa10ece2fe8c
Adam Long <adam.jacob.long@gmail.com>
2024-10-16 11:22:45 -0500

    SCRUM-105 added styles

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -4,8 +4,8 @@ import SignIn from "../../../components/SignIn/SignIn";
 export default function Home() {
 
     return (
-        <>
+        <div className="flex-center size-full max-sm:px-6 min-h-screen">
             <SignIn/>
-        </>
+        </div>
     );
 }
