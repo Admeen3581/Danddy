69971208ce16b8c95f084b753c895d38614d04d6
Olivia Laurel <zsw26@txstate.edu>
2024-10-16 16:14:46 -0500

    Adding in sliding menu

--- PlayerHeaderComponents/PlayerHeader/playerheader.tsx
+++ PlayerHeaderComponents/PlayerHeader/playerheader.tsx
@@ -5,12 +5,22 @@ import Link from 'next/link';
 const PlayerHeader = () => {
   return (
     <div className="PAHome-section">
-       <div className="headerBox">
-          <h1>PLAYER HOME PAGE</h1>
-          <Link href="/combat" className="button">
-            Combat
-          </Link>
-       </div>
+      <div className="headerBox">
+        {/* The sliding menu on top of the header */}
+        <div className="menu-container">
+          <button className="menu-button">☰</button>
+          <div className="menu-content">
+            <ul>
+              <li><Link href="/combat">Combat</Link></li>
+              <li><Link href="/stats">Stats</Link></li>
+              <li><Link href="/inventory">Inventory</Link></li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Player home page title */}
+        <h1>PLAYER HOME PAGE</h1>
+      </div>
     </div>
   );
 };
