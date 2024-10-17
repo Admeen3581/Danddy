296b053c33ff2d5af50477d047123df16b93626c
Lorenz De Robles <pvd8@txstate.edu>
2024-10-17 02:58:05 -0500

    SCRUM-137 eliminated box shadows and added missing skills/attributes

--- components/Combat/Skills/wisSkills.tsx
+++ components/Combat/Skills/wisSkills.tsx
@@ -5,6 +5,7 @@ const WisSkills = () => {
   const stats = {
     animal: 11,
     insight: 10,
+    medicine: 10,
     perception: 10,
     survival: 10
   };
@@ -15,6 +16,7 @@ const WisSkills = () => {
       <div className={styles.statsBox}>
         <Stat label="Animal Handling" value={stats.animal} />
         <Stat label="Insight" value={stats.insight} />
+        <Stat label="Medicine" value={stats.medicine} />
         <Stat label="Perception" value={stats.perception} />
         <Stat label="Survival" value={stats.survival} />
       </div>
