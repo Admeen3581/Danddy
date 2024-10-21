f127f4e5cfa5be054b4c5078f21b4054ec11d53c
Adam Long <adam.jacob.long@gmail.com>
2024-10-21 16:10:16 -0500

    SCRUM-105 fix to conflict

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -8,8 +8,8 @@ import SignInLogic from "../../../components/authorization/SignInLogic";
 export default function Home() {
 
     return (
-        <div className="flex-center size-full max-sm:px-6 min-h-screen">
-            <SignInLogic/>
-        </div>
+        <>
+            <SignUp/>
+        </>
     );
 }
