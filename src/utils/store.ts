00bdef3cf0f5258a9b5f2c36cff7adb359884863
Roman <cvu10@txstate.edu>
2024-10-21 14:19:51 -0500

    Another attempt to fix merge conflicts

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,8 +1,11 @@
 import { create } from 'zustand';
+import { Character, createBlankCharacterJSON } from './characterJsonFunctions';
 
 interface LocalState {
     roomId: String;
     setRoomId: (roomId: String) => void;
+    classesJson: Character
+    setClassesJson: (classesJson: Character) => void
     userId: String;
     setUserId: (userId: String) => void;
 }
@@ -10,8 +13,11 @@ interface LocalState {
 const useLocalStore = create<LocalState>((set) => ({
     roomId: "",
     setRoomId: (roomId: String) => set({ roomId }),
+    classesJson: createBlankCharacterJSON(),
+    setClassesJson: (classesJson: Character) => set({classesJson}),
     userId: "",
     setUserId: (userId: String) => set({ userId }),
 }));
 
 export default useLocalStore;
+
