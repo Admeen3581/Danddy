e710b8e94200e89836d77e4c5ebf08304b4e8512
Olivia Laurel <zsw26@txstate.edu>
2024-10-21 12:29:12 -0500

    Adding in text box for dms

--- DmComponents/DMNotes/dmnotes.tsx
+++ DmComponents/DMNotes/dmnotes.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './dmnotes.css';
 
 const DMNotes = () => {
+  const [notes, setNotes] = useState('');
+
+  const handleNotesChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNotes(e.target.value);
+  };
+
   return (
     <div className="dmhome-section">
        <div className="NotesBox">
-            <h1>Notes:</h1>
+        <h1>Notes:</h1>
+        <textarea 
+          className="textarea-notes"
+          value={notes}
+          onChange={handleNotesChange}
+          placeholder="Type your notes here..."
+        />
        </div>
     </div>
   );
