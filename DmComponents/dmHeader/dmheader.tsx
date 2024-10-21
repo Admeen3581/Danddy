ee4fa9dbe013a3eefb82ac7e63741556d3da61a2
Olivia Laurel <zsw26@txstate.edu>
2024-10-21 12:22:27 -0500

    Adding in side menu for dms

--- DmComponents/dmHeader/dmheader.tsx
+++ DmComponents/dmHeader/dmheader.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './dmheader.css';
+import Link from 'next/link';
 
 const DMHeader = () => {
   return (
     <div className="dmhome-section">
        <div className="headerBox">
+       <div className="dmmenu-container">
+          <button className="dmmenu-button">☰</button>
+          <div className="dmmenu-content">
+            <ul>
+              <li><Link href="/combat">Combat</Link></li>
+              <li><Link href="/stats">Stats</Link></li>
+              <li><Link href="/inventory">Inventory</Link></li>
+            </ul>
+          </div>
+        </div>
+
           <h1>DM HOME PAGE</h1>
        </div>
     </div>
