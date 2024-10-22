39f9ee66c86847f54e05c65a89add4ee0ba43e75
Joshua Mason <kon9@txstate.edu>
2024-10-21 20:52:08 -0500

    SCRUM-129 - Added skills to Charisma

--- components/Combat/Skills/chaSkills.tsx
+++ components/Combat/Skills/chaSkills.tsx
@@ -1,12 +1,23 @@
+import useLocalStore from "@/utils/store";
 import Stat from "../../../CharSheetsCompon/CSStats/Stat";
 import styles from './chaSkills.module.css'
+import { getModifier } from "@/utils/characterJsonFunctions";
 
 const ChaSkills = () => {
+  const { classesJson, setClassesJson } = useLocalStore();
+  
+  var numStats = []
+
+  for(var stat in classesJson.stats.charisma.skills){
+    if(classesJson.stats.charisma.skills[stat]) numStats.push(getModifier(classesJson.stats.charisma.value)+2);
+    else numStats.push(getModifier(classesJson.stats.charisma.value))
+  }
+
   const stats = {
-    deception: 11,
-    intimidation: 10,
-    performance: 10,
-    persuasion: 10
+    deception: (numStats[0] <= 0 ? numStats[0] : "+"+numStats[0]),
+    intimidation: (numStats[1] <= 0 ? numStats[1] : "+"+numStats[1]),
+    performance: (numStats[2] <= 0 ? numStats[2] : "+"+numStats[2]),
+    persuasion: (numStats[3] <= 0 ? numStats[3] : "+"+numStats[3])
   };
 
   return (
