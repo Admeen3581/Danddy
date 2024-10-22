eb6404e726d39fd2c3f4392bd8f81cb1ea62f7a5
Joshua Mason <kon9@txstate.edu>
2024-10-21 21:12:19 -0500

    SCRUM-129 - Added stats to player sheet

--- PlayerHeaderComponents/PlayerStats/playerstats.tsx
+++ PlayerHeaderComponents/PlayerStats/playerstats.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './playerstats.css';
+import { getModifier } from '@/utils/characterJsonFunctions';
+import { readDatabaseRoute } from '@/utils/httpRequester';
+import useLocalStore from '@/utils/store';
 
 const PlayerStats = () => {
+  const { classesJson, setClassesJson } = useLocalStore();
+  readDatabaseRoute("characters/testerCharacterCreation")
+    .then((result) => {
+      setClassesJson(result)
+    })
+    
   return (
     <div className="PAstats-home"> 
       <div className="StatsBox">
-        <span className="inline-text">Strength</span>
-        <span className="inline-text">Dexterity</span>
-        <span className="inline-text">Constitution</span>
-        <span className="inline-text">Intelligence</span>
-        <span className="inline-text">Wisdom</span>
-        <span className="inline-text">Charisma</span>
+        <span className="inline-text">Strength: {getModifier(classesJson.stats.strength.value)}</span>
+        <span className="inline-text">Dexterity: {getModifier(classesJson.stats.dexterity.value)}</span>
+        <span className="inline-text">Constitution: {getModifier(classesJson.stats.constitution.value)}</span>
+        <span className="inline-text">Intelligence: {getModifier(classesJson.stats.intelligence.value)}</span>
+        <span className="inline-text">Wisdom: {getModifier(classesJson.stats.wisdom.value)}</span>
+        <span className="inline-text">Charisma: {getModifier(classesJson.stats.charisma.value)}</span>
       </div>
     </div>
   );
