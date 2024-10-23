5cde98500cf752778a2ed5e0cfcc832cfb676b1c
Adam Long <adam.jacob.long@gmail.com>
2024-10-22 23:03:13 -0500

    SCRUM-105 hotfix

--- components/authorization/AuthForm.tsx
+++ components/authorization/AuthForm.tsx
@@ -34,7 +34,7 @@ export default function AuthForm({type} : {type : string}) {
   // Submission handler
   const onSubmit = (values: z.infer<typeof formSchema>) =>
   {
-    console.log("its submitting");//test log -- remove before prod -- security issue
+    console.log("its submitting");//debugger -- remove before prod -- security issue
     //Authenticate user here
     setIsLoading(true);
     try
