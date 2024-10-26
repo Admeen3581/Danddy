fb6b428dd2cae9bf99724f78add7ff48adf8b389
Adam Long <adam.jacob.long@gmail.com>
2024-10-26 18:40:00 -0500

    SCRUM-112 implement global button state

--- DmComponents/DMButtons/dmbuttons.tsx
+++ DmComponents/DMButtons/dmbuttons.tsx
@@ -1,20 +1,32 @@
 import './dmbuttons.css';
 import Link from 'next/link';
 import {MessagePopUp} from "@/components/messageDrawer";
+import {messageButtonState} from "@/lib/messageButtonState";
 
 const DMButtons = () => {
+
+    const [enabled, setEnabled] = messageButtonState();
+
+    const openMessageButtonState = () => {
+        setEnabled(true);
+    }
+
+    const closeMessageButtonState = () => {
+        setEnabled(false);
+    }
 
   return (
     <div className="dmhome-section">
        <div className="ButtonsBox">
           <h1>Buttons:</h1>
           <div className="button-container">
-          <Link href="/chars" className="button"> {}
-            Characters
-          </Link>
-          <button className='button'>NPC</button>
-          <button className='button'>Enemy</button>
+              <Link href="/chars" className="button"> {}
+                  Characters
+              </Link>
+              <button className='button'>NPC</button>
+              <button className='button'>Enemy</button>
               <MessagePopUp/>
+              <button onClick={openMessageButtonState}>Direct Message</button>
           </div>
        </div>
     </div>
