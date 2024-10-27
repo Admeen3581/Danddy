f15efb0430ddd4ff12ff9b2119eb0d220a3daaa6
Olivia Laurel <zsw26@txstate.edu>
2024-10-26 19:42:40 -0500

    SCRUM-118 Add in map display

---
+++ DmComponents/DMMapDisplay/mapdis.tsx
@@ -0,0 +1,14 @@
+import './mapdis.css';
+
+const DMButtons = () => {
+
+  return (
+   <div className="container">
+      <div className="MapsDis">
+         <h1>Maps:</h1>
+      </div>
+    </div>
+  );
+};
+
+export default DMButtons;
