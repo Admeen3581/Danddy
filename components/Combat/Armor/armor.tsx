45cb69254cafab8542a5a54e248c8a8324405ab1
Lorenz De Robles <pvd8@txstate.edu>
2024-10-26 19:54:46 -0500

    SCRUM-140 added styles and modifications to armor and ac

--- components/Combat/Armor/armor.tsx
+++ components/Combat/Armor/armor.tsx
@@ -1,11 +1,11 @@
 import Stat from "../../../CharSheetsCompon/CSStats/Stat";
 import styles from './armor.module.css'
 
-const SensesStats = () => {
+const Armor = () => {
   
   return (
     <div className={styles.stats}>
-      <h2 className={styles.statsTitle}>Senses</h2>
+      <h2 className={styles.statsTitle}>AC & Armor</h2>
       <div className={styles.statsBox}>
         <Stat label="Armor Class" value="16" />
         <Stat label="Initiative" value="+1" />
@@ -14,4 +14,4 @@ const SensesStats = () => {
   );
 };
 
-export default SensesStats;
+export default Armor;
