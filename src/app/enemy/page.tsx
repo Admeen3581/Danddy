70f7c2ca230a5f99ad71e1e995189a540a773146
Olivia Laurel <zsw26@txstate.edu>
2024-10-27 13:30:12 -0500

    SCRUM-118 Adding in enemy home code

---
+++ src/app/enemy/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import EnemyHeader from '../../../EnemyComponents/EnemyHeader/enemyheader';
+
+
+const EnemyHome = () => {
+  return (
+   <EnemyHeader/>
+  );
+};
+
+export default EnemyHome;
