d4de6f0edb8196a3431431a0dae0c83554e099bc
Adam Long <adam.jacob.long@gmail.com>
2024-10-29 15:19:04 -0500

    SCRUM-157 implement final touches

--- src/components/directMessagePopup.tsx
+++ src/components/directMessagePopup.tsx
@@ -20,15 +20,15 @@ import { DoorOpen } from 'lucide-react';
 import {ScrollArea} from "@/components/ui/scroll-area";
 
 type convo = {
-    id: number;
+    uid: number;
     user: string;
     content: string;
 };
 
 const tempConvos = [
-    { id: 1, user: 'Alice the wicked witch', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
-    { id: 2, user: 'Bob', content: 'Are you free to chat?' },
-    { id: 3, user: 'Charlie', content: 'Let’s meet up tomorrow.' },
+    { uid: 1, user: 'Alice the wicked witch', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+    { uid: 2, user: 'Bob', content: 'Are you free to chat?' },
+    { uid: 3, user: 'Charlie', content: 'Let’s meet up tomorrow.' },
 ];
 
 export function DirectMessagePopup({style})
@@ -72,16 +72,16 @@ export function DirectMessagePopup({style})
                                 </div>
                             </div>
                             <br/>
-                            <ScrollArea>
-                                <div className='message-content'>
+                            <div className='message-content'>
+                                <ScrollArea>
                                     <div className="message incoming">
                                         <p className="content">{selectedMessage.content}</p>
                                     </div>
                                     <div className="message outgoing">
                                         <span className="content">This is a response message!</span>
                                     </div>
-                                </div>
-                            </ScrollArea>
+                                </ScrollArea>
+                            </div>
                         </div>
                         <div className="input-container">
                             <input
@@ -98,18 +98,19 @@ export function DirectMessagePopup({style})
                     <div className="conversations-container">
                         <ScrollArea>
                             {tempConvos.map(convo => (
-                                <div key={convo.id} className="conversation-preview" onClick={() => handleSelectConvo(convo)}>
-                                <span className="user">
-                                    {convo.user.slice(0,21)}
-                                    {convo.user.length > 21 && (
-                                        "..."
-                                    )}
-                                </span>
+                                <div key={convo.uid} className="conversation-preview" onClick={() => handleSelectConvo(convo)}>
+                                    <span className="user">
+                                        {convo.user.slice(0,21)}
+                                        {convo.user.length > 21 && (
+                                            "..."
+                                        )}
+                                    </span>
                                     <p className="preview-content">
-                                        {convo.content.slice(0,30)}
+                                        {convo.content.slice(0,30)} {/*change to most recent message*/}
                                         {convo.content.length > 30 && (
                                             "..."
-                                        )}</p>
+                                        )}
+                                    </p>
                                 </div>
                             ))}
                         </ScrollArea>
