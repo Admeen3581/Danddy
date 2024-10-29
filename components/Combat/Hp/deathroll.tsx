09b43b5ce4a15857052e2caa5f63f3486f46d416
Lorenz De Robles <pvd8@txstate.edu>
2024-10-29 17:58:18 -0500

    SCRUM-125 Added functions for death and save

--- components/Combat/Hp/deathroll.tsx
+++ components/Combat/Hp/deathroll.tsx
@@ -1,7 +1,23 @@
-const DeathComponent = () => (
-  <div>
-    <h1>You have died</h1>
-  </div>
-);
+import { useState } from "react";
+
+const DeathComponent = () => {
+  const [deathCount, setDeathCount] = useState(0); 
+  const [saveCount, setSaveCount] = useState(0);
+
+  const handleSave = () => {
+    setSaveCount((prevCount) => prevCount + 1);
+  };
+
+  const handleNewDeath = () => {
+    setDeathCount((prevCount) => prevCount + 1);
+  };
+
+
+   return (
+   <div>
+     <h1>You have died</h1>
+   </div>
+   );
+};
 
 export default DeathComponent;
