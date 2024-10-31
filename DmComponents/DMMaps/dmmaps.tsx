2e1bc731c7a2f78aa7cb7c70efa2a3b6c1357a5c
Olivia Laurel <zsw26@txstate.edu>
2024-10-31 13:05:43 -0500

    SCRUM-122 Scrollability on maps

--- DmComponents/DMMaps/dmmaps.tsx
+++ DmComponents/DMMaps/dmmaps.tsx
@@ -1,11 +1,21 @@
 import './dmmaps.css';
 
 const DMButtons = () => {
-
   return (
     <div className="MapsBox">
-        <h1>Maps:</h1>
-        
+      <h1>Maps:</h1>
+      <div className="scrollable-content">
+        <p>Map 1: beach</p>
+        <p>Map 2: mannor</p>
+        <p>Map 3: dragons den</p>
+        <p>Map 4: meeting room</p>
+        <p>Map 5: etc</p>
+        <p>Map 6: etc</p>
+        <p>Map 7: etc</p>
+        <p>Map 8: etc</p>
+        <p>Map 9: etc</p>
+        <p>Map 10: etc</p>
+      </div>
     </div>
   );
 };
