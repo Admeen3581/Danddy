b8bf70a41c9646acb275f2af95c61256a95cc79d
Olivia Laurel <zsw26@txstate.edu>
2024-11-03 20:50:52 -0600

    SCRUM-120 Deleting unused body

--- DmComponents/DMHeader/dmheader.tsx
+++ DmComponents/DMHeader/dmheader.tsx
@@ -2,33 +2,35 @@ import React from 'react';
 import './dmheader.css';
 import Link from 'next/link';
 import { MessagePopUp } from "@/components/messageDrawer";
-import {DirectMessagePopup} from "@/components/directMessagePopup";
+import { DirectMessagePopup } from "@/components/directMessagePopup";
 
 const DMHeader = () => {
   return (
-    <div className="dmhome-section">
-       <div className="headerBox">
-       <div className="dmmenu-container">
-          <button className="dmmenu-button">☰</button>
-          <div className="dmmenu-content">
-            <ul>
+    <div className="background">  {/* Added background wrapper */}
+      <div className="dmhome-section">
+        <div className="headerBox">
+          <div className="dmmenu-container">
+            <button className="dmmenu-button">☰</button>
+            <div className="dmmenu-content">
+              <ul>
                 <li><Link href="/combat">Characters</Link></li>
                 <li><Link href="/NPC">NPC</Link></li>
                 <li><Link href="/enemy">Enemy</Link></li>
-                <MessagePopUp/>
-                <DirectMessagePopup style={'button'}/>
-            </ul>
+                <MessagePopUp />
+                <DirectMessagePopup style={'button'} />
+              </ul>
+            </div>
           </div>
-        </div>
 
-        <h1 className="dmglow-text">
-          <span>DM</span>
-          <span> </span>
-          <span>HOME</span>
-          <span> </span>
-          <span>PAGE</span>
-        </h1>
-       </div>
+          <h1 className="dmglow-text">
+            <span>DM</span>
+            <span> </span>
+            <span>HOME</span>
+            <span> </span>
+            <span>PAGE</span>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
