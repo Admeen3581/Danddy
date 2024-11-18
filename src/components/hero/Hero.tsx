0f36acf50e6b852516a584b87b79698943463c14
Olivia Laurel <zsw26@txstate.edu>
2024-11-17 23:02:05 -0600

    SCRUM-182 added invalid room code handlers

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,23 +1,41 @@
-import React, { ChangeEvent, useRef } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 import './Hero.css';
 import Link from 'next/link';
 import useLocalStore from '@/utils/store';
 
 const Hero = () => {
-
-  const {roomId, setRoomId} = useLocalStore()
+  const { roomId, setRoomId } = useLocalStore();
   const entryBoxRef = useRef<HTMLInputElement>(null);
+  const [isValid, setIsValid] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setRoomId(event.target.value);
+    const value = event.target.value.trim();
+    setRoomId(value);
+
+
+    if (value.match(/^[a-zA-Z0-9]{6}$/)) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+
+    // Reset error message visibility when typing
+    setShowErrorMessage(false);
   };
 
+  const handleJoinClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (!isValid) {
+      event.preventDefault(); // Prevent navigation if the room code is invalid
+      setShowErrorMessage(true); // Show the error message
+    }
+  };
 
   return (
     <div className="hero-section">
       <div className="hero-content">
         <h1>DANDDY</h1>
-        <p></p>
+        <p>Welcome to Danddy!</p>
 
         <input
           type="text"
@@ -26,13 +44,18 @@ const Hero = () => {
           placeholder="Enter your room code..."
           className="hero-input"
         />
+        {showErrorMessage && <p className="error-message">Invalid room code.</p>}
 
-        <Link href="/playerhome" className="hero-button">
-            Join
+        <Link
+          href={isValid ? "/playerhome" : "#"}
+          className={`hero-button ${!isValid ? 'disabled' : ''}`}
+          onClick={handleJoinClick}
+        >
+          Join
         </Link>
         <Link href="/dmhome" className="hero-button">
-            Create
-          </Link>
+          Create
+        </Link>
       </div>
     </div>
   );
